52361ee6d4d730a50d91cb8ce8b2abc7c4cdd6a7
Mohamed <a2410042@institutmontilivi.cat>
2026-01-18 16:02:06 +0100

    creacio de una peticio Post

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,18 @@ const express = require('express'); //require = funcion importar librerias
                                      //express = libreria para crear servidores
 const app = express(); //app = objeto para crear servidores
 
+app.use(express.json()); //ayuda que el servidor entienda json
+
 // req = request (lo que entra)
 // res = response (lo que sale)
 app.get('/', (req, res) => { 
     res.send('Hello World!'); //mensaje de prueba
+});
+
+app.post('/analizar-vendedor', (req, res) => { 
+    console.log(req.body);
+    // Lógica para analizar el vendedor
+    res.send('dades rebudes correctament');
 });
 
 //app.listen = funcion para arrancar el servidor 3000 el puerto del server
