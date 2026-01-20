c05cb2dffc5d1638370c9d913bda82bce20e73f5
Mohamed <a2410042@institutmontilivi.cat>
2026-01-20 10:34:29 +0100

    Millores vendedor services

--- backend/services/vendedorService.js
+++ backend/services/vendedorService.js
@@ -1,15 +1,27 @@
-
 const calcularPuntos = (datosVendedor) => { //funcion de caluclo de puntos del vendedor
     let puntos = 0; //variable para guardar puntos del vendedor que se mostraran al usuario
     // Lógica para calcular los puntos del vendedor
-    if (datosVendedor.telefono) {
+    if (datosVendedor.telefono) { //si tiene telefono sumar 30 puntos
         puntos += 30;
     }
-    if (datosVendedor.web) {
+    if (datosVendedor.web) { //si tiene web sumar 30 puntos
         puntos += 30;
     }
-    if (puntos > 100) {
+    if (datosVendedor.precioMuyBajo === true) { //si tiene precio muy bajo restar 25 puntos
+        puntos -= 25;
+    }
+    if (datosVendedor.reportes && datosVendedor.reportes > 0) { //si tiene reportes restar 20 puntos por cada reporte
+        puntos -= datosVendedor.reportes * 20;
+    }
+    if (!datosVendedor.telefono && !datosVendedor.web) { //si no tiene telefono ni web restar 30 puntos
+        puntos -= 30;
+    }
+
+    if (puntos > 100) { //si puntos superan 100 puntos, puntos = 100
         puntos = 100;
+    }
+    if (puntos < 0) { //si puntos son negativos, puntos = 0
+        puntos = 0;
     }
     return puntos; //retornar puntos
 }
@@ -28,13 +40,19 @@ const interpretarPuntos = (puntos) => { //funcion de interpretacion de puntos de
         mensaje = 'Vendedor poco fiable, no recomendado';
         color = 'rojo';
     }
-    return {mensaje, color}; //retornar mensaje y color
+    return {
+        mensaje,
+        color
+    }; //retornar mensaje y color
 };
 
 const analizarVendedorService = (datosVendedor) => { //funcion de caluclo de puntos del vendedor
     const puntos = calcularPuntos(datosVendedor); //llamar a función de calcular puntos
     const resultado = interpretarPuntos(puntos); //llamar a función de interpretar puntos
-    return {puntos, resultado}; //retornar puntos y mensaje y color
+    return {
+        puntos,
+        resultado
+    }; //retornar puntos y mensaje y color
 };
 
 const exportar = module.exports = {
